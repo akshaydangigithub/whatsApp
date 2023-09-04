bd241fedfca2eba891398c40ed4a1044590db4fe
hariom dangi <113966179+akshaydangigithub@users.noreply.github.com>
2023-09-04 17:11:36 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var plm = require("passport-local-mongoose");
 
 // mongoose.connect("mongodb+srv://hdangi224:hdangi224@cluster0.gilhcih.mongodb.net/facebook-clone?retryWrites=true&w=majority");
 
-momngoose.connect(
+mongoose.connect(
   "mongodb+srv://hdangi224:C5x37OozR9uEys9j@cluster0.lrh9oam.mongodb.net/whatsApp?retryWrites=true&w=majority"
 );
 var userSchema = mongoose.Schema(
